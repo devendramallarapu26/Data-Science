26b1a0a4912760e6cbb30b18044297a4c9c96732
Devendra <devendramallarapu8@gmail.com>
2024-12-08 14:39:26 +0530

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -1,3 +1,9 @@
 import numpy as np
 a = np.arange(1,11)
 print(a.ndim,a.size,a.dtype)
+print(np.arange(1,13).reshape(3,4))
+output:
+(1, 10, dtype('int32'))
+array([[ 1,  2,  3,  4],
+       [ 5,  6,  7,  8],
+       [ 9, 10, 11, 12]])
